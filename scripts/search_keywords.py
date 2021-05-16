60e2e386e366c12645bf0f1a4e8514708ac33943
Henry Valbuena <dev.henryvm@gmail.com>
2021-05-15 21:46:43 -0400

    refactor search keywords module

--- scripts/search_keywords.py
+++ scripts/search_keywords.py
@@ -1,35 +1,21 @@
-import re
-
-pairings = {
-    "pair1": ["ps5", "stock"],
-    "pair2": ["ps5", "available"],
-    "pair3": ["playstation", "available"],
-    "pair4": ["playstation", "stock"],
-}
+pairings = [
+    ["ps5", "stock"],
+    ["ps5", "available"],
+    ["playstation", "available"],
+    ["playstation", "stock"],
+]
 
 
-def contains_words(text):
-    return (
-        (
-            re.search(pairings["pair1"][0], text, re.I)
-            and re.search(pairings["pair1"][1], text, re.I)
-        )
-        or (
-            re.search(pairings["pair2"][0], text, re.I)
-            and re.search(pairings["pair2"][1], text, re.I)
-        )
-        or (
-            re.search(pairings["pair3"][0], text, re.I)
-            and re.search(pairings["pair3"][1], text, re.I)
-        )
-        or (
-            re.search(pairings["pair4"][0], text, re.I)
-            and re.search(pairings["pair4"][1], text, re.I)
-        )
-    )
+def contains_words(pairs, text):
+    text_lc = text.lower()
+    for a, b in pairs:
+        if a.lower() in text_lc and b.lower() in text_lc:
+            return True
+
+    return False
 
 
 def is_in_stock(data_list):
     for data in data_list:
-        if contains_words(data):
+        if contains_words(pairings, data):
             return True
