d18922321ac9dc76b903d98b877ff9321403eb76
Henry Valbuena <dev.henryvm@gmail.com>
2021-05-16 21:59:40 -0400

    added new user

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from scripts.search_keywords import is_in_stock
 from scripts.send_discord_alert import send_alert
 
 
-users = ["https://twitter.com/EBGamesCanada", "https://twitter.com/BBYC_Gamers"]
+users = [
+    "https://twitter.com/EBGamesCanada",
+    "https://twitter.com/BBYC_Gamers",
+    "https://twitter.com/WalmartCAGaming",
+]
 
 for user in users:
     tweets = get_tweets(user)
